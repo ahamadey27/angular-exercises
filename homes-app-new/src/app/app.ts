74c1ddb7b325930afad6cb734eb334359835f9b0
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-04 15:48:51 -0400

    feat: implement home component and update app layout
    - Added HomeComponent to the application with a simple template.
    - Updated AppComponent to include the HomeComponent in the main layout.
    - Changed the title of the application from 'default' to 'homes'.
    - Introduced a new JSON configuration file for Chrome DevTools.

--- homes-app-new/src/app/app.ts
+++ homes-app-new/src/app/app.ts
@@ -1,13 +1,21 @@
 import {Component} from '@angular/core';
+import { HomeComponent} from './home/home.component';
 
 @Component({
   selector: 'app-root',
   imports: [],
   template: `
-    <h1>Default</h1>
-  `,
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>`,
   styleUrls: ['./app.css'],
+  imports: [HomeComponent]
 })
 export class App {
-  title = 'default';
+  title = 'homes';
 }
