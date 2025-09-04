d54fb9004f7deb21197755196f8d88100efd9698
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-04 17:04:20 -0400

    feat: add HousingLocation component and interface
    - Created HousingLocationInterface to define the structure of housing location data.
    - Implemented HousingLocation component with input property for housing location.
    - Updated component template to display housing location details including image, heading, and location.

--- homes-app-new/src/app/housing-location/housing-location.ts
+++ homes-app-new/src/app/housing-location/housing-location.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { HousingLocationInterface } from '../housing-location-interface';
 
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
+  imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img class="listing-photo">
+      <h2 class="listing-heading"></h2>
+      <p class="listing-location"></p>
+    </section>
   `,
   styles: ``
 })
 export class HousingLocation {
+  @Input() housingLocation!: HousingLocationInterface;
 
 }
+ 
