7411e61572f903f3bca15df65d63d7f63fb4cb27
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-04 16:31:38 -0400

    feat: add HousingLocation component with basic template and styles

---
+++ homes-app-new/src/app/housing-location/housing-location.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styles: ``
+})
+export class HousingLocation {
+
+}
