a9d2d41cf5e994056eaef77a08b9161d34658db2
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-11 11:23:39 -0400

    feat: Correct interface name to 'Product' and add optional description property

--- ts-basics/interfaces.ts
+++ ts-basics/interfaces.ts
@@ -1,7 +1,17 @@
-interface product
+interface Product
 {
     name: string;
     price: number;
     getCategories: () => string[];
+    description?: string;
 
+}
+
+class Keyboard implements Product {
+    name: string = 'Keyboard';
+    price: number = 120;
+
+    getCategories(): string[] {
+        return ['Computing', 'Peripherals']
+    }
 }
