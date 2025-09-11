9439dfee69714ae02787942906ee33908aafd953
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-11 12:44:32 -0400

    feat: Add product listing to display items when products are available

--- my-app/src/app/product-list/product-list.ts
+++ my-app/src/app/product-list/product-list.ts
@@ -9,7 +9,14 @@ import { Product } from '../product';
   templateUrl: './product-list.html',
   styleUrls: ['./product-list.css']
 })
+
+// product array will be used to store a list of Product objects
 export class ProductList {
-  products: Product[] = []; // product array will be used to store a list of Product objects
+  products: Product[] = [
+    {id: 1, title: 'Keyboard'},
+    {id: 2, title: 'Microphone'},
+    {id: 3, title: 'Web camera'},
+    {id: 4, title: 'Tablet'}
+  ]; 
 
 }
