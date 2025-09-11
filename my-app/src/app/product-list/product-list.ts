df283587286f317c29f05845b6377f14ce6e3860
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-09-11 12:28:18 -0400

    feat: Update product list template and integrate CommonModule for improved functionality

--- my-app/src/app/product-list/product-list.ts
+++ my-app/src/app/product-list/product-list.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.html',
   styleUrls: ['./product-list.css']
 })
 export class ProductList {
+  products: Product[] = []; // product array will be used to store a list of Product objects
 
 }
